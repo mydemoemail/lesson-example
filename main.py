cbb557a0491b36ca6df0158e8475c5b2bbb935db
Dmitriy <dmitriy.shchebetovskyi@gmail.com>
2023-07-08 12:40:15 +0300

    Added conditions part1

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 # git init - створювання нового репозиторiю
 # git commit -m "commit message" - створення нового коммiта
 
+# умови
+# v1
+# n1 = 10
+# n2 = 20
+# v2
+# n1, n2 = 10, 20  # множинне привласнення
+# print(n1 > n2)
+# print(n1 >= n2)
+# print(n1 < n2)
+# print(n1 <= n2)
+# print(n1 == n2)  # поверне True якщо обидва операнди рівні (однакові)
+# print(n1 != n2)  # поверне True якщо обидва операнди різні
 
+# print(1 == 1 and 3 == 3)  # поверне True якщо обидва операнди рівні True, інакше False
+# print(1 == 10 or 2 == 3)  # поверне True якщо хоча б один операнд дорівнює True, інакше False
+# #
+# is_valid = False
+# print(is_valid)
+# print(not is_valid)  # not -> інверсія, якщо значення False стане True, і навпаки
+# #
+# print("hello" in "hello world")
 
